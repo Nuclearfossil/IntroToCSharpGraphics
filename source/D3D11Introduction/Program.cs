2e0eb45ea3a8baab822349e4cd637647b28b92b0
Ash Matheson <nuclearfossil@hotmail.com>
2017-02-18 18:30:33 -0800

    Initial D3D introduction via SharpDX

---
+++ source/D3D11Introduction/Program.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Windows.Forms;
+
+using SharpDX;
+using SharpDX.D3DCompiler;
+using SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using SharpDX.Windows;
+using SharpDX.Direct3D;
+using System.Diagnostics;
+
+namespace D3D11Introduction
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            RenderForm form = new RenderForm("D3D11 Introduction");
+
+            SwapChainDescription swapChainDesc = new SwapChainDescription()
+            {
+                BufferCount = 1,
+                ModeDescription = new ModeDescription(form.ClientSize.Width, form.ClientSize.Height,
+                                                      new Rational(60, 1),
+                                                      Format.R8G8B8A8_UNorm),
+                IsWindowed = true,
+                OutputHandle = form.Handle,
+                SampleDescription = new SampleDescription(1, 0),
+                Usage = Usage.RenderTargetOutput
+            };
+
+            SharpDX.Direct3D11.Device device;
+            SwapChain swapChain;
+            SharpDX.Direct3D11.Device.CreateWithSwapChain(SharpDX.Direct3D.DriverType.Hardware, DeviceCreationFlags.None, swapChainDesc, out device, out swapChain);
+            DeviceContext context = device.ImmediateContext;
+
+
+            Factory factory = swapChain.GetParent<Factory>();
+            factory.MakeWindowAssociation(form.Handle, WindowAssociationFlags.IgnoreAll);
+
+            CompilationResult vertexShaderResult = ShaderBytecode.CompileFromFile("example01.fx", "VS", "vs_4_0");
+            VertexShader vertexShader = new VertexShader(device, vertexShaderResult);
+
+            CompilationResult pixelShaderResult = ShaderBytecode.CompileFromFile("example01.fx", "PS", "ps_4_0");
+            PixelShader pixelShader = new PixelShader(device, pixelShaderResult);
+
+            var signature = ShaderSignature.GetInputSignature(vertexShaderResult);
+            var layout = new InputLayout(device, signature, new[]
+                            {
+                                new InputElement("POSITION", 0, Format.R32G32B32A32_Float, 0, 0),
+                                new InputElement("COLOR", 0, Format.R32G32B32A32_Float, 16, 0)
+                            });
+
+            var vertices = SharpDX.Direct3D11.Buffer.Create(device, BindFlags.VertexBuffer, new[]
+                                  {
+                                      new Vector4(-1.0f, -1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f), // Front
+                                      new Vector4(-1.0f,  1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f),
+                                      new Vector4( 1.0f,  1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f),
+                                      new Vector4(-1.0f, -1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f),
+                                      new Vector4( 1.0f,  1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f),
+                                      new Vector4( 1.0f, -1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f),
+
+                                      new Vector4(-1.0f, -1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 0.0f, 1.0f), // BACK
+                                      new Vector4( 1.0f,  1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 0.0f, 1.0f),
+                                      new Vector4(-1.0f,  1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 0.0f, 1.0f),
+                                      new Vector4(-1.0f, -1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 0.0f, 1.0f),
+                                      new Vector4( 1.0f, -1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 0.0f, 1.0f),
+                                      new Vector4( 1.0f,  1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 0.0f, 1.0f),
+
+                                      new Vector4(-1.0f, 1.0f, -1.0f,  1.0f), new Vector4(0.0f, 0.0f, 1.0f, 1.0f), // Top
+                                      new Vector4(-1.0f, 1.0f,  1.0f,  1.0f), new Vector4(0.0f, 0.0f, 1.0f, 1.0f),
+                                      new Vector4( 1.0f, 1.0f,  1.0f,  1.0f), new Vector4(0.0f, 0.0f, 1.0f, 1.0f),
+                                      new Vector4(-1.0f, 1.0f, -1.0f,  1.0f), new Vector4(0.0f, 0.0f, 1.0f, 1.0f),
+                                      new Vector4( 1.0f, 1.0f,  1.0f,  1.0f), new Vector4(0.0f, 0.0f, 1.0f, 1.0f),
+                                      new Vector4( 1.0f, 1.0f, -1.0f,  1.0f), new Vector4(0.0f, 0.0f, 1.0f, 1.0f),
+
+                                      new Vector4(-1.0f,-1.0f, -1.0f,  1.0f), new Vector4(1.0f, 1.0f, 0.0f, 1.0f), // Bottom
+                                      new Vector4( 1.0f,-1.0f,  1.0f,  1.0f), new Vector4(1.0f, 1.0f, 0.0f, 1.0f),
+                                      new Vector4(-1.0f,-1.0f,  1.0f,  1.0f), new Vector4(1.0f, 1.0f, 0.0f, 1.0f),
+                                      new Vector4(-1.0f,-1.0f, -1.0f,  1.0f), new Vector4(1.0f, 1.0f, 0.0f, 1.0f),
+                                      new Vector4( 1.0f,-1.0f, -1.0f,  1.0f), new Vector4(1.0f, 1.0f, 0.0f, 1.0f),
+                                      new Vector4( 1.0f,-1.0f,  1.0f,  1.0f), new Vector4(1.0f, 1.0f, 0.0f, 1.0f),
+
+                                      new Vector4(-1.0f, -1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 1.0f, 1.0f), // Left
+                                      new Vector4(-1.0f, -1.0f,  1.0f, 1.0f), new Vector4(1.0f, 0.0f, 1.0f, 1.0f),
+                                      new Vector4(-1.0f,  1.0f,  1.0f, 1.0f), new Vector4(1.0f, 0.0f, 1.0f, 1.0f),
+                                      new Vector4(-1.0f, -1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 1.0f, 1.0f),
+                                      new Vector4(-1.0f,  1.0f,  1.0f, 1.0f), new Vector4(1.0f, 0.0f, 1.0f, 1.0f),
+                                      new Vector4(-1.0f,  1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 1.0f, 1.0f),
+
+                                      new Vector4( 1.0f, -1.0f, -1.0f, 1.0f), new Vector4(0.0f, 1.0f, 1.0f, 1.0f), // Right
+                                      new Vector4( 1.0f,  1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 1.0f, 1.0f),
+                                      new Vector4( 1.0f, -1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 1.0f, 1.0f),
+                                      new Vector4( 1.0f, -1.0f, -1.0f, 1.0f), new Vector4(0.0f, 1.0f, 1.0f, 1.0f),
+                                      new Vector4( 1.0f,  1.0f, -1.0f, 1.0f), new Vector4(0.0f, 1.0f, 1.0f, 1.0f),
+                                      new Vector4( 1.0f,  1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 1.0f, 1.0f),
+                            });
+
+            var constantBuffer = new SharpDX.Direct3D11.Buffer(device, 
+                                                            Utilities.SizeOf<Matrix>(), 
+                                                            ResourceUsage.Default, 
+                                                            BindFlags.ConstantBuffer, 
+                                                            CpuAccessFlags.None, 
+                                                            ResourceOptionFlags.None, 0);
+
+            context.InputAssembler.InputLayout = layout;
+            context.InputAssembler.PrimitiveTopology = PrimitiveTopology.TriangleList;
+            context.InputAssembler.SetVertexBuffers(0, new VertexBufferBinding(vertices, Utilities.SizeOf<Vector4>() * 2, 0));
+            context.VertexShader.SetConstantBuffer(0, constantBuffer);
+            context.VertexShader.Set(vertexShader);
+            context.PixelShader.Set(pixelShader);
+
+            Matrix view = Matrix.LookAtLH(new Vector3(0, 0, -5), new Vector3(0, 0, 0), Vector3.UnitY);
+            Matrix proj = Matrix.Identity;
+
+            Texture2D backBuffer = null;
+            RenderTargetView renderView = null;
+            Texture2D depthBuffer = null;
+            DepthStencilView depthView = null;
+
+            bool hasUserResized = true;
+
+            Stopwatch clock = new Stopwatch();
+            clock.Start();
+
+            form.UserResized += (sender, args) => { hasUserResized = true; };
+
+            form.KeyUp += (sender, args) =>
+            {
+                // Do interesting stuff with the keyboard
+                if (args.KeyCode == Keys.Escape)
+                {
+                    form.Close();
+                }
+            };
+
+            RenderLoop.Run(form, () =>
+            {
+                if (hasUserResized)
+                {
+                    Utilities.Dispose(ref backBuffer);
+                    Utilities.Dispose(ref renderView);
+                    Utilities.Dispose(ref depthBuffer);
+                    Utilities.Dispose(ref depthView);
+
+                    swapChain.ResizeBuffers(swapChainDesc.BufferCount, form.ClientSize.Width, form.ClientSize.Height, Format.Unknown, SwapChainFlags.None);
+
+                    backBuffer = Texture2D.FromSwapChain<Texture2D>(swapChain, 0);
+                    renderView = new RenderTargetView(device, backBuffer);
+                    depthBuffer = new Texture2D(device, new Texture2DDescription()
+                    {
+                        Format = Format.D32_Float_S8X24_UInt,
+                        ArraySize = 1,
+                        MipLevels = 1,
+                        Width = form.ClientSize.Width,
+                        Height = form.ClientSize.Height,
+                        SampleDescription = new SampleDescription(1, 0),
+                        Usage = ResourceUsage.Default,
+                        BindFlags = BindFlags.DepthStencil,
+                        CpuAccessFlags = CpuAccessFlags.None,
+                        OptionFlags = ResourceOptionFlags.None
+                    });
+
+                    depthView = new DepthStencilView(device, depthBuffer);
+
+                    context.Rasterizer.SetViewport(new Viewport(0, 0, form.ClientSize.Width, form.ClientSize.Height, 0.0f, 1.0f));
+                    context.OutputMerger.SetTargets(depthView, renderView);
+
+                    proj = Matrix.PerspectiveFovLH((float)Math.PI / 4.0f, (float)form.ClientSize.Width / (float)form.ClientSize.Height, 0.1f, 100.0f);
+
+                    hasUserResized = false;
+                }
+
+                Matrix viewProj = Matrix.Multiply(view, proj);
+
+                context.ClearDepthStencilView(depthView, DepthStencilClearFlags.Depth, 1.0f, 0);
+                context.ClearRenderTargetView(renderView, Color.Black);
+
+                float currentTime = clock.ElapsedMilliseconds / 1000.0f;
+                Matrix worldViewProj = Matrix.RotationX(currentTime) * Matrix.RotationY(currentTime * 2.0f) * Matrix.RotationZ(currentTime * 0.7f) * viewProj;
+                worldViewProj.Transpose();
+                context.UpdateSubresource(ref worldViewProj, constantBuffer);
+
+                context.Draw(36, 0);
+
+                swapChain.Present(0, PresentFlags.None);
+            });
+
+            signature.Dispose();
+            vertexShaderResult.Dispose();
+            vertexShader.Dispose();
+            pixelShaderResult.Dispose();
+            pixelShader.Dispose();
+            vertices.Dispose();
+            layout.Dispose();
+            constantBuffer.Dispose();
+            depthBuffer.Dispose();
+            renderView.Dispose();
+            backBuffer.Dispose();
+            context.ClearState();
+            context.Flush();
+            device.Dispose();
+            context.Dispose();
+            swapChain.Dispose();
+            factory.Dispose();
+        }
+    }
+}
---
+++ source/SharpDXIntroduction/SharpMesh.cs
@@ -0,0 +1,184 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX;
+using SharpDX.DXGI;
+using SharpDX.Direct3D11;
+using SharpDX.D3DCompiler;
+using SharpDX.Direct3D;
+
+using Buffer11 = SharpDX.Direct3D11.Buffer;
+using Device = SharpDX.Direct3D11.Device;
+using MapFlags = SharpDX.Direct3D11.MapFlags;
+using System.IO;
+
+namespace SharpDXIntroduction
+{
+    /// <summary>
+    /// To Render Static Object
+    /// </summary>
+    public class SharpMesh : IDisposable
+    {
+
+        /// <summary>
+        /// Device pointer
+        /// </summary>
+        public SharpDevice Device { get; private set; }
+
+        /// <summary>
+        /// Vertex Buffer
+        /// </summary>
+        public Buffer11 VertexBuffer { get; private set; }
+
+        /// <summary>
+        /// Index Buffer
+        /// </summary>
+        public Buffer11 IndexBuffer { get; private set; }
+
+        /// <summary>
+        /// Vertex Size
+        /// </summary>
+        public int VertexSize { get; private set; }
+
+        /// <summary>
+        /// Mesh Parts
+        /// </summary>
+        public List<SharpSubSet> SubSets { get; private set; }
+
+        private SharpMesh(SharpDevice device)
+        {
+            Device = device;
+            SubSets = new List<SharpSubSet>();
+        }
+
+
+        /// <summary>
+        /// Draw Mesh
+        /// </summary>
+        public void Draw()
+        {
+            Device.DeviceContext.InputAssembler.PrimitiveTopology = PrimitiveTopology.TriangleList;
+            Device.DeviceContext.InputAssembler.SetVertexBuffers(0, new VertexBufferBinding(VertexBuffer, VertexSize, 0));
+            Device.DeviceContext.InputAssembler.SetIndexBuffer(IndexBuffer, Format.R32_UInt, 0);
+            Device.DeviceContext.DrawIndexed(SubSets[0].IndexCount, 0, 0);
+        }
+
+
+        /// <summary>
+        /// Draw Mesh
+        /// </summary>
+        /// <param name="subset">Subsets</param>
+        public void Draw(int subset)
+        {
+            Device.DeviceContext.DrawIndexed(SubSets[subset].IndexCount, SubSets[subset].StartIndex, 0);
+        }
+
+        /// <summary>
+        /// Create From Vertices and Indices array
+        /// </summary>
+        /// <typeparam name="VType">Vertex Type</typeparam>
+        /// <param name="device">Device</param>
+        /// <param name="vertices">Vertices</param>
+        /// <param name="indices">Indices</param>
+        /// <returns>Mesh</returns>
+        public static SharpMesh Create<VType>(SharpDevice device, VType[] vertices, int[] indices) where VType : struct
+        {
+            SharpMesh mesh = new SharpMesh(device);
+            mesh.VertexBuffer = Buffer11.Create<VType>(device.Device, BindFlags.VertexBuffer, vertices);
+            mesh.IndexBuffer = Buffer11.Create(device.Device, BindFlags.IndexBuffer, indices);
+            mesh.VertexSize = SharpDX.Utilities.SizeOf<VType>();
+            mesh.SubSets.Add(new SharpSubSet()
+            {
+                DiffuseColor = new Vector4(1, 1, 1, 1),
+                IndexCount = indices.Count()
+            });
+            return mesh;
+        }
+
+
+        /// <summary>
+        /// Create a quad for Multiple Render Target
+        /// </summary>
+        /// <param name="device">Device</param>
+        /// <returns>Mesh</returns>
+        public static SharpMesh CreateQuad(SharpDevice device)
+        {
+            Vector3[] vertices = new Vector3[]
+            {
+                new Vector3(-1, 1, 0),
+                new Vector3(-1, -1, 0),
+                new Vector3(1, 1, 0),
+                new Vector3(1, -1, 0)
+            };
+
+            int[] indices = new int[] { 0, 2, 1, 2, 3, 1 };
+            SharpMesh mesh = new SharpMesh(device);
+            mesh.VertexBuffer = Buffer11.Create<Vector3>(device.Device, BindFlags.VertexBuffer, vertices.ToArray());
+            mesh.IndexBuffer = Buffer11.Create(device.Device, BindFlags.IndexBuffer, indices.ToArray());
+            mesh.VertexSize = SharpDX.Utilities.SizeOf<Vector3>();
+
+            mesh.SubSets.Add(new SharpSubSet()
+            {
+                DiffuseColor = new Vector4(1, 1, 1, 1),
+                IndexCount = indices.Count()
+            });
+
+            return mesh;
+        }
+
+        /// <summary>
+        /// Set all buffer and topology property to speed up rendering
+        /// </summary>
+        public void Begin()
+        {
+            Device.DeviceContext.InputAssembler.PrimitiveTopology = PrimitiveTopology.TriangleList;
+            Device.DeviceContext.InputAssembler.SetVertexBuffers(0, new VertexBufferBinding(VertexBuffer, VertexSize, 0));
+            Device.DeviceContext.InputAssembler.SetIndexBuffer(IndexBuffer, Format.R32_UInt, 0);
+        }
+
+        /// <summary>
+        /// Draw all vertices as points
+        /// </summary>
+        /// <param name="count"></param>
+        public void DrawPoints(int count = int.MaxValue)
+        {
+            Device.DeviceContext.InputAssembler.PrimitiveTopology = PrimitiveTopology.PointList;
+            Device.DeviceContext.InputAssembler.SetVertexBuffers(0, new VertexBufferBinding(VertexBuffer, VertexSize, 0));
+            Device.DeviceContext.InputAssembler.SetIndexBuffer(IndexBuffer, Format.R32_UInt, 0);
+            Device.DeviceContext.DrawIndexed(Math.Min(count, SubSets[0].IndexCount), 0, 0);
+        }
+
+        /// <summary>
+        /// Draw patch
+        /// </summary>
+        /// <param name="topology">Patch Topology type</param>
+        public void DrawPatch(PrimitiveTopology topology)
+        {
+            Device.DeviceContext.InputAssembler.PrimitiveTopology = topology;
+            Device.DeviceContext.InputAssembler.SetVertexBuffers(0, new VertexBufferBinding(VertexBuffer, VertexSize, 0));
+            Device.DeviceContext.InputAssembler.SetIndexBuffer(IndexBuffer, Format.R32_UInt, 0);
+            Device.DeviceContext.DrawIndexed(SubSets[0].IndexCount, 0, 0);
+        }
+
+        /// <summary>
+        /// Release resource
+        /// </summary>
+        public void Dispose()
+        {
+            VertexBuffer.Dispose();
+            IndexBuffer.Dispose();
+            foreach (var s in SubSets)
+            {
+                if (s.DiffuseMap != null)
+                    s.DiffuseMap.Dispose();
+
+                if (s.NormalMap != null)
+                    s.NormalMap.Dispose();
+            }
+        }
+    }
+}
+
